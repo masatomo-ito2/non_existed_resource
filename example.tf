ce526b17489cc0265dacefcb8ca49b395733040b
masa3 <masa@hashicorp.com>
2019-09-11 01:00:33 +0900

    renamed file

--- example.tf
+++ example.tf
@@ -1,13 +1,13 @@
 
 data "local_file" "resource_not_exist" {
-  filename = "${path.module}/not_exist.bar"
+  filename = "${path.module}/not_exist.txt"
 
   depends_on = [local_file.resource_not_exist]
 }
 
 resource "local_file" "resource_not_exist" {
   content  = "foo! This is a file created."
-  filename = "${path.module}/not_exist.bar"
+  filename = "${path.module}/not_exist.txt"
 }
 
 output "filename" {
