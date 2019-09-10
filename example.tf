1f5a469b868c08cdde34cf20ecfaf0e5a8a46428
masa3 <masa@hashicorp.com>
2019-09-11 00:04:36 +0900

    name change

--- example.tf
+++ example.tf
@@ -2,20 +2,20 @@ variable "file_count" {
   default = 1
 }
 
-data "local_file" "file_not_exist" {
+data "local_file" "resource_not_exist" {
 	count = "${var.file_count}" 
 	filename = "${path.module}/not_exist.bar"
 
-	depends_on = [local_file.file_not_exist]
+	depends_on = [local_file.resource_not_exist]
 }
 
-resource "local_file" "file_not_exist" {
+resource "local_file" "resource_not_exist" {
 	count = "${var.file_count}" 
 	content  = "foo! This is file created: ${count.index}"
   filename = "${path.module}/not_exist.bar"
 }
 
 output "filename" {
-  value = "${local_file.file_not_exist.*.filename}"
+  value = "${local_file.resource_not_exist.*.filename}"
 }
 
